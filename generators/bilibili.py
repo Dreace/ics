67dece3716626e6b0f660ff114e3f045e3d26eb9
Dreace <Dreace@Foxmail.com>
2021-02-27 22:41:19 +0800

    保留过去一周的番剧更新时间

--- generators/bilibili.py
+++ generators/bilibili.py
@@ -1,6 +1,7 @@
+import math
 import uuid
 from datetime import datetime, timedelta
-import math
+
 import icalendar
 import requests
 from dateutil.relativedelta import relativedelta
@@ -13,7 +14,16 @@ utc_tz = timezone('UTC')
 
 
 def bilibili(bilibili_config: Bilibili):
+    uid_list = {}
     calendar = icalendar.Calendar()
+    calendar.update()
+    with open('./ics/bilibili.ics', 'rb') as file:
+        c = icalendar.Calendar.from_ical(file.read())
+        for component in c.walk():
+            if component.name == "VEVENT":
+                if datetime.now(tz=cst_tz) - component.get('dtstart').dt < timedelta(days=14):
+                    uid_list[(str(component.get('uid')))] = component
+                    calendar.add_component(component)
     timeline = get_timeline()
     for i in range(math.ceil(bilibili_config.limit / 30)):
         limit = 30
@@ -27,23 +37,31 @@ def bilibili(bilibili_config: Bilibili):
             if timeline.get(bangumi['season_id'], -1) == -1:
                 continue
             title = f'{bangumi["title"]} {timeline[bangumi["season_id"]]["index"]}'
+            uid = str(uuid.uuid5(uuid.NAMESPACE_OID, title)) + '@Dreace'
+
             datetime_start = datetime.fromtimestamp(timeline[bangumi['season_id']]['time'], cst_tz)
             datetime_end = datetime_start + relativedelta(minutes=30)
+            event: icalendar.Event = uid_list.get(uid, None)
+            if event:
+                del event['dtstart']
+                del event['dtend']
+                event.add('dtstart', datetime_start)
+                event.add('dtend', datetime_end)
+            else:
+                event = icalendar.Event()
+                event.add('X-WR-TIMEZONE', 'Asia/Shanghai')
+                event.add('uid', uid)
+                event.add('summary', title)
+                event.add('dtstart', datetime_start)
+                event.add('dtend', datetime_end)
+                event.add('TRIGGER', timedelta(minutes=-10))
 
-            event = icalendar.Event()
-            event.add('X-WR-TIMEZONE', 'Asia/Shanghai')
-            event.add('uid', str(uuid.uuid5(uuid.NAMESPACE_OID, title)) + '@Dreace')
-            event.add('summary', title)
-            event.add('dtstart', datetime_start)
-            event.add('dtend', datetime_end)
-            event.add('TRIGGER', timedelta(minutes=-10))
-
-            # 开播十分钟前提醒
-            alarm = icalendar.Alarm()
-            alarm.add("TRIGGER", timedelta(minutes=-10))
-            alarm.add("ACTION", "DISPLAY")
-            event.add_component(alarm)
-            calendar.add_component(event)
+                # 开播十分钟前提醒
+                alarm = icalendar.Alarm()
+                alarm.add("TRIGGER", timedelta(minutes=-10))
+                alarm.add("ACTION", "DISPLAY")
+                event.add_component(alarm)
+                calendar.add_component(event)
     with open('./ics/bilibili.ics', 'wb') as ics_file:
         # 去掉换行
         ics_file.write(calendar.to_ical().replace('\r\n '.encode(), b''))
